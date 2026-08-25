795755cf222a38b04e7df6101cf2ff5898bd246e
Prasanna <pbalamurugan2@sheffield.ac.uk>
2026-08-25 21:23:59 +0100

    Modified solution

--- solutions/q51-q100/q051_prime_digit_replacements.py
+++ solutions/q51-q100/q051_prime_digit_replacements.py
@@ -13,7 +13,7 @@ being the first member of this family, is the smallest prime with this property.
 Find the smallest prime which, by replacing part of the number (not necessarily
 adjacent digits) with the same digit, is part of an eight prime value family.
 
-Measured Runtime: ~0.105000s
+Measured Runtime: ~0.056000s
 """
 
 import time
